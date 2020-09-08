315b3ca794fb56172dc09370f79a93b308610214
Curtis Holt <29793215+curtisjamesholt@users.noreply.github.com>
2020-09-08 12:46:55 +0100

    Added transformation functions.

--- easybpy.py
+++ easybpy.py
@@ -181,6 +181,20 @@ def object_exists(ref):
             return True
         else:
             return False
+
+def rename_object(obj, newname):
+    objref = None
+    # obj is string
+    if is_string(obj):
+        objref = get_object(obj)
+    else:
+        objref = obj
+    # set name - only if string
+    if is_string(newname):
+        objref.name = newname
+        return True
+    else:
+        return False
 #endregion
 #region OBJECTS - PRIMITIVES
 def create_cube():
@@ -312,11 +326,130 @@ def display_as_wire(ref):
             return False
 #endregion
 #region TRANSFORMATIONS
-def location(obj = None, newloc = None):
-    pass
-def rotation(obj = None, newrot = None):
-    pass
-def scale(obj = None, newscale = None):
+def location(obj = None, loc = None):
+
+    # set up vars
+    objref = None
+    obj_provided = False
+    loc_provided = False
+
+    # obj checks
+    if obj != None:
+        obj_provided = True
+        # obj has been provided
+        if is_string(obj):
+            objref = get_object(obj)
+        else:
+            objref = obj
+
+    # loc checks
+    if loc != None:
+        # loc has been provided
+        loc_provided = True
+
+    if obj_provided:
+        # obj has been provided
+        if loc_provided:
+            # case 1 - obj and loc provided
+            objref.location = Vector((loc[0],loc[1],loc[2]))
+        else:
+            # case 2 - obj but no loc provided
+            return objref.location
+    else:
+        # obj has not been provided
+        if loc_provided:
+            # case 3 - obj not provided but loc is
+            objref = so()
+            objref.location = Vector((loc[0],loc[1],loc[2]))
+        else:
+            # case 4 - no obj and no loc provided
+            return so().location
+
+def rotation(obj = None, rot = None):
+
+    # set up vars
+    objref = None
+    obj_provided = False
+    rot_provided = False
+
+    # obj checks
+    if obj != None:
+        obj_provided = True
+        # obj has been provided
+        if is_string(obj):
+            objref = get_object(obj)
+        else:
+            objref = obj
+
+    # newloc checks
+    if rot != None:
+        # rot has been provided
+        rot_provided = True
+
+    if obj_provided:
+        # obj has been provided
+        if rot_provided:
+            # case 1 - obj and rot provided
+            objref.rotation_euler[0] = rot[0]
+            objref.rotation_euler[1] = rot[1]
+            objref.rotation_euler[2] = rot[2]
+        else:
+            # case 2 - obj but no rot provided
+            return objref.rotation_euler
+    else:
+        # obj has not been provided
+        if rot_provided:
+            # case 3 - obj not provided but rot is
+            objref = so()
+            objref.rotation_euler[0] = rot[0]
+            objref.rotation_euler[1] = rot[1]
+            objref.rotation_euler[2] = rot[2]
+        else:
+            # case 4 - no obj and no rot provided
+            return so().rotation_euler
+            
+def scale(obj = None, scale = None):
+
+    # set up vars
+    objref = None
+    obj_provided = False
+    scale_provided = False
+
+    # obj checks
+    if obj != None:
+        obj_provided = True
+        # obj has been provided
+        if is_string(obj):
+            objref = get_object(obj)
+        else:
+            objref = obj
+
+    # newloc checks
+    if scale != None:
+        # rot has been provided
+        scale_provided = True
+
+    if obj_provided:
+        # obj has been provided
+        if scale_provided:
+            # case 1 - obj and scale provided
+            objref.scale[0] = scale[0]
+            objref.scale[1] = scale[1]
+            objref.scale[2] = scale[2]
+        else:
+            # case 2 - obj but no scale provided
+            return objref.scale
+    else:
+        # obj has not been provided
+        if scale_provided:
+            # case 3 - obj not provided but scale is
+            objref = so()
+            objref.scale[0] = scale[0]
+            objref.scale[1] = scale[1]
+            objref.scale[2] = scale[2]
+        else:
+            # case 4 - no obj and no scale provided
+            return so().scale
     pass
 #endregion
 #region 3D CURSOR
